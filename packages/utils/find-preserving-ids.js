4b8c58a57813eac3a858d276405aa84c72754c23
Pascal Kaufmann <kaufmae@me.com>
2021-12-10 16:46:01 +0100

    Fix findPreservingIds should skip addFields pipeline stage if index is not used
    # Conflicts:
    #	packages/utils/src/find-preserving-ids.js

--- packages/utils/find-preserving-ids.js
+++ packages/utils/find-preserving-ids.js
@@ -8,37 +8,39 @@ const sortBySequence = {
   sequence: 1,
 };
 
-export default (Collection) => async (selector, ids, options = {}) => {
-  const defaultSort = AMAZON_DOCUMENTDB_COMPAT_MODE
-    ? sortBySequence
-    : sortByIndex;
+const defaultSort = AMAZON_DOCUMENTDB_COMPAT_MODE
+  ? sortBySequence
+  : sortByIndex;
 
-  const { skip, limit, sort = defaultSort } = options;
-  const filteredSelector = {
-    ...selector,
-    _id: { $in: ids },
-  };
-  const filteredPipeline = [
-    {
-      $match: filteredSelector,
-    },
-    !AMAZON_DOCUMENTDB_COMPAT_MODE && {
-      $addFields: {
-        index: { $indexOfArray: [ids, '$_id'] },
+export default (Collection) =>
+  async (selector, ids, options = {}) => {
+    const { skip, limit, sort = defaultSort } = options;
+    const filteredSelector = {
+      ...selector,
+      _id: { $in: ids },
+    };
+
+    const filteredPipeline = [
+      {
+        $match: filteredSelector,
       },
-    },
-    sort && { $sort: sort },
-    skip && { $skip: skip },
-    limit && { $limit: limit },
-  ].filter(Boolean);
+      sort?.index && {
+        $addFields: {
+          index: { $indexOfArray: [ids, '$_id'] },
+        },
+      },
+      sort && { $sort: sort },
+      skip && { $skip: skip },
+      limit && { $limit: limit },
+    ].filter(Boolean);
 
-  const rawCollection = Collection.rawCollection();
-  const aggregateCollection = Meteor.wrapAsync(
-    rawCollection.aggregate,
-    rawCollection
-  );
+    const rawCollection = Collection.rawCollection();
+    const aggregateCollection = Meteor.wrapAsync(
+      rawCollection.aggregate,
+      rawCollection
+    );
 
-  const aggregationPointer = aggregateCollection(filteredPipeline);
-  const items = await aggregationPointer.toArray();
-  return (items || []).map((item) => new Collection._transform(item)); // eslint-disable-line
-};
+    const aggregationPointer = aggregateCollection(filteredPipeline);
+    const items = await aggregationPointer.toArray();
+    return (items || []).map((item) => new Collection._transform(item)); // eslint-disable-line
+  };
